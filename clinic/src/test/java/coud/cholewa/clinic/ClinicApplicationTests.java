f4d0900e79bba847f714ee9a0fb422cde33a634e
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-26 20:46:08 +0100

    added basic structure for all application as modules

---
+++ clinic/src/test/java/coud/cholewa/clinic/ClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package coud.cholewa.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

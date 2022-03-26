b7f709890d941bd7968df695a110e6e1a14482bf
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-26 20:55:00 +0100

    [CLI-1] added patient entity

---
+++ clinic/src/main/java/coud/cholewa/clinic/patient/model/Patient.java
@@ -0,0 +1,10 @@
+package coud.cholewa.clinic.patient.model;
+
+public class Patient {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String details;
+}

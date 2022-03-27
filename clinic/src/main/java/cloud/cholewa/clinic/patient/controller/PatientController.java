955e16d6b0caa529f8a542a5ab01fecc5ea2abc1
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-27 21:26:49 +0200

    [CLI-5] added updatePatient

--- clinic/src/main/java/cloud/cholewa/clinic/patient/controller/PatientController.java
+++ clinic/src/main/java/cloud/cholewa/clinic/patient/controller/PatientController.java
@@ -2,6 +2,7 @@ package cloud.cholewa.clinic.patient.controller;
 
 import cloud.cholewa.clinic.patient.dto.PatientRequest;
 import cloud.cholewa.clinic.patient.dto.PatientResponse;
+import cloud.cholewa.clinic.patient.dto.UpdatePatientRequest;
 import cloud.cholewa.clinic.patient.service.PatientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -43,5 +46,10 @@ public class PatientController {
     public ResponseEntity<Void> deletePatient(@PathVariable String pesel) {
         patientService.deletePatientByPesel(pesel);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/{pesel}")
+    public ResponseEntity<PatientResponse> updatePatient(@PathVariable String pesel, @Valid @RequestBody UpdatePatientRequest updatePatientRequest) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(patientService.updatePatient(pesel, updatePatientRequest));
     }
 }
--- clinic/src/main/java/cloud/cholewa/clinic/patient/dto/UpdatePatientRequest.java
+++ clinic/src/main/java/cloud/cholewa/clinic/patient/dto/UpdatePatientRequest.java
@@ -5,20 +5,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class PatientRequest {
+public class UpdatePatientRequest {
 
     private static final String PHONE_PATTERN = "(?:(?:(?:\\+|00)?48)|(?:\\(\\+?48\\)))?(?:1[2-8]|2[2-69]|3[2-49]|4[1-8]|5[0-9]|6[0-35-9]|[7-8][1-9]|9[145])\\d{7}";
     private static final String PESEL_PATTERN = "^[0-9]{11}$";
-
-    @NotBlank(message = "Missing pesel field")
-    @Pattern(regexp = PESEL_PATTERN, message = "Invalid pesel field")
-    private String pesel;
 
     @NotBlank(message = "Missing firstName field")
     @Size(min = 2, message = "firstName field cant be shorten that 2")
@@ -32,5 +29,7 @@ public class PatientRequest {
     @Pattern(regexp = PHONE_PATTERN, message = "Invalid phone field")
     private String phone;
 
+    @NotNull(message = "Missing details field")
+    @Size(max = 1000, message = "exceeded description max length 1000")
     private String details;
 }

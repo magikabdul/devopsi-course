54d3fa366421a08e8a1d17468ab6a543b85d6bef
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-29 00:30:05 +0200

    [EXC-3] export to file

--- exchange/src/main/java/cloud/cholewa/exchange/controller/ExchangeController.java
+++ exchange/src/main/java/cloud/cholewa/exchange/controller/ExchangeController.java
@@ -4,17 +4,23 @@ import cloud.cholewa.exchange.dto.ExchangeRateResponse;
 import cloud.cholewa.exchange.model.Rate;
 import cloud.cholewa.exchange.service.ExchangeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+
+import static cloud.cholewa.exchange.utils.DateTimeUtils.getCurrentDateInISO;
 
 @RestController
 @RequestMapping("/currencies")
 @RequiredArgsConstructor
+@Slf4j
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
@@ -35,5 +41,19 @@ public class ExchangeController {
                 sourceValue,
                 targetCurrency,
                 date));
+    }
+
+    @GetMapping("/toFile")
+    public ResponseEntity<Void> getAllRatesByAndStoreToFile(HttpServletResponse response, @RequestParam(required = false) String date) {
+        String queryDate = date != null ? date : getCurrentDateInISO();
+        response.setContentType("text/csv");
+        response.addHeader("Content-Disposition","attachment; filename=\"" + queryDate + "---rates.csv\"");
+        try {
+            exchangeService.createCSVFileWithAllRatesByDate(response.getWriter(), date);
+        } catch (IOException e) {
+            log.error("exchangeService.createCSVFileWithAllRatesByDate(response.getWriter(), date)");
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().build();
     }
 }

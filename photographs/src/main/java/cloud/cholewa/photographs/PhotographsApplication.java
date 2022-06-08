23d0f6e2d193bdb31e49d393aac6556953ba9c95
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-06-08 20:35:32 +0200

    [PHO-1] initial configuration

---
+++ photographs/src/main/java/cloud/cholewa/photographs/PhotographsApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.photographs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotographsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotographsApplication.class, args);
+    }
+
+}
---
+++ photographs/src/test/java/cloud/cholewa/photographs/PhotographsApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.photographs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotographsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
